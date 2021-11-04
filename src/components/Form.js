f2fc932fd5cfd4c0146432a34015a45d45f931ee
efoejean <77749280+efoejean@users.noreply.github.com>
2021-11-03 20:36:52 -0500

    create a Form file and white the form html and export it

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+export default `
+<form class="flex flex-col gap-2">
+  <div>
+    <label class="sr-only" for="search">Search</label>
+    <input type="search" placeholder="Search..." id="search" />
+  </div>
+  <div>
+    <label for="in-stock-only">Only show products in stock</label>
+    <input type="checkbox" id="in-stock-only" />
+  </div>
+</form>`;
